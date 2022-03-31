60b234118ce1b4a26b08bc32c8745ef5d8004a3e
elijahsamuels <elijahsamuels@gmail.com>
2022-03-31 11:59:32 -0600

    added copyright field. completionBonusReturn function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import Calculator from './components/calculator.js';
+import "./App.css";
+import Calculator from "./components/calculator.js";
 
 function App() {
+
+	const d = new Date();
+	let year = d.getFullYear();
+	
   return (
     <div className="App">
-			<Calculator />
+      <Calculator />
 
-
+      <div>©{year} < a href="mailto:elijahsamuels@gmail.com">Elijah Samuels</a>
     </div>
+	</div> 
   );
 }
 
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -95,6 +95,18 @@ function Calculator() {
 		setTaxRateMarkup(e.target.value.includes("%") ? (parseInt(e.target.value)/100 || 0 ) : (parseInt(e.target.value)/100 || 0 ))
 	}
 
+	console.log(commissionBonus)
+	const completionBonusReturn = () => {
+		if (commissionBonus > 0) {
+			return <tr>
+				<td align="left">Completion Bonus {commissionBonus}%
+					{" "}:</td>
+				<td align="left">+${Math.floor((totalPriceMarkup - totalPrice)*(commissionBonus/100))}</td>
+				<td align="left">+${Math.floor((totalPriceMarkup - totalPrice)*(commissionBonus/100))}</td>
+			</tr>
+		} 
+	}
+
   return (
     <React.Fragment>
       <h1>Calculator</h1>
@@ -243,6 +255,43 @@ function Calculator() {
           </tr>
 
           <tr>
+            {/* <td align="left">Completion Commission</td> */}
+						<td align="left">Completion Bonus? <select value={commissionBonus} onChange={(e) => setCommissionBonus(e.target.value)}>
+								<option value="0">0%</option>
+								<option value="1">1%</option>
+								<option value="2">2%</option>
+								<option value="3">3%</option>
+								<option value="4">4%</option>
+								<option value="5">5%</option>
+								<option value="6">6%</option>
+								<option value="7">7%</option>
+								<option value="8">8%</option>
+								<option value="9">9%</option>
+								<option value="10">10%</option>
+								<option value="11">11%</option>
+								<option value="12">12%</option>
+								<option value="13">13%</option>
+								<option value="14">14%</option>
+								<option value="15">15%</option>
+								<option value="16">16%</option>
+								<option value="17">17%</option>
+								<option value="18">18%</option>
+								<option value="19">19%</option>
+								<option value="20">20%</option>
+							</select>
+							{" "}:</td>
+            <td>
+              <input
+								type="number"
+                disabled
+                name="completion-commission"
+                placeholder={`$${commission2}`}
+                onChange={(e) => setCommission2(e.target.value || 0)}
+              />
+            </td>
+          </tr>
+
+          <tr>
             <td align="left">Tax Rate:</td>
             <td>
               <input
@@ -351,34 +400,13 @@ function Calculator() {
             <td align="left">+${commission2}</td>
           </tr>
 
-          <tr>
-            <td align="left">Completion Bonus? <select value={commissionBonus} onChange={(e) => setCommissionBonus(e.target.value)}>
-								<option value="0">0%</option>
-								<option value="1">1%</option>
-								<option value="2">2%</option>
-								<option value="3">3%</option>
-								<option value="4">4%</option>
-								<option value="5">5%</option>
-								<option value="6">6%</option>
-								<option value="7">7%</option>
-								<option value="8">8%</option>
-								<option value="9">9%</option>
-								<option value="10">10%</option>
-								<option value="11">11%</option>
-								<option value="12">12%</option>
-								<option value="13">13%</option>
-								<option value="14">14%</option>
-								<option value="15">15%</option>
-								<option value="16">16%</option>
-								<option value="17">17%</option>
-								<option value="18">18%</option>
-								<option value="19">19%</option>
-								<option value="20">20%</option>
-							</select>
+					{completionBonusReturn()}
+          {/* <tr>
+            <td align="left">Completion Bonus {commissionBonus}%
 							{" "}:</td>
             <td align="left">+${Math.floor((totalPriceMarkup - totalPrice)*(commissionBonus/100))}</td>
             <td align="left">+${Math.floor((totalPriceMarkup - totalPrice)*(commissionBonus/100))}</td>
-          </tr>
+          </tr> */}
 
           <tr>
             <td align="left">Subtotal: </td>
