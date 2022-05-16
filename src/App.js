463e2cd2e1d973dfc6681cbc06af2a5c9c5b06ea
elijahsamuels <elijahsamuels@gmail.com>
2022-05-16 16:58:31 -0600

    added react router dom. added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import "./App.css";
 import Calculator from "./components/calculator.js";
+import BandCalculator from "./components/bandcalculator.js";
+import Spreadsheet from "./components/spreadsheet.js";
+import Footer from "./components/footer.js";
+import NavBar from "./components/navbar.js";
+import About from "./pages/about.js";
+import Home from "./pages/home.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
-	const d = new Date();
-	let year = d.getFullYear();
-	
   return (
     <div className="App">
-      <Calculator />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="spreadsheet" element={<Spreadsheet />} />
+        <Route path="bandcalculator" element={<BandCalculator />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <h3 align="center" style={{ color: "red" }}>
+                There's nothing here!
+              </h3>
+            </main>
+          }
+        />
+      </Routes>
 
-      <div>©{year} < a href="mailto:elijahsamuels@gmail.com">Elijah Samuels</a>
+      <Footer />
     </div>
-	</div> 
   );
 }
 
