1f4acfd2e11114d3e882d1c3a8f9dc4b641cff10
elijahsamuels <elijahsamuels@gmail.com>
2022-05-16 16:55:54 -0600

    added home page"

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+	return (
+<React.Fragment>
+		<div>Home page</div>
+		<div>Home page</div>
+		<div>Home page</div>
+		<div>Home page</div>
+</React.Fragment>
+	)
+}
+
+export default Home
